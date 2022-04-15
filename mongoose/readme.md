ec1d29c83b4e8bafca31b3cff11d6892bbdc2fca
harishkumarforever <h.nunavath@dev2prod.com>
2022-04-15 17:16:13 +0530

    mongoose is in process

---
+++ mongoose/readme.md
@@ -0,0 +1,10 @@
+moogoose
+
+1. npm link ==> https://www.npmjs.com/package/mongoose
+
+2. offical link ==> https://mongoosejs.com/
+
+3. all attrubutes on scheme Object
+   https://mongoosejs.com/docs/schematypes.html
+
+4. status codes https://www.webfx.com/web-development/glossary/http-status-codes/
---
+++ mongoose/src/db/task.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/TaskManagerAPI");
+
+const Task = mongoose.model("tasks", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const task1 = new Task({
+  description: "ffddsf",
+  completed: true,
+});
+
+const main = async () => {
+  try {
+    const res = await task1.save();
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main();
